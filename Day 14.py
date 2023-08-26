f50ec8fca3e643c98edf337e09ae2aefaada4d45
Israa Yousuf <141610564+wahaibisraa@users.noreply.github.com>
2023-08-26 19:45:14 +0400

    Add files via upload

---
+++ Day 14.py
@@ -0,0 +1,24 @@
+def evaluate_postfix(expression):
+    stack = []
+
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            if char == '+':
+                stack.append(operand1 + operand2)
+            elif char == '-':
+                stack.append(operand1 - operand2)
+            elif char == '*':
+                stack.append(operand1 * operand2)
+            elif char == '/':
+                stack.append(operand1 / operand2)
+
+    return stack[0]
+
+expression = input("Enter a postfix expression: ")
+
+result = evaluate_postfix(expression)
+print("Result:", result)
